907a82838f822004e4b7f190fba1d36e698c5183
Nagapriya-mca <nagapriya1109@gmail.com>
2024-09-28 14:44:30 +0530

    Delete index.php

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = htmlspecialchars($_POST['name']);
-    $email = htmlspecialchars($_POST['email']);
-    $message = htmlspecialchars($_POST['message']);
-    // Process the data (e.g., save to database, send email, etc.)
-}
-?>
